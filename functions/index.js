912ea2a2770ab0708eff5920e487828470f5f64e
ritxon <richardssgm@gmail.com>
2021-05-26 19:18:15 +1000

    cors error

--- functions/index.js
+++ functions/index.js
@@ -5,6 +5,9 @@ admin.initializeApp();
 
 const express = require("express");
 const app = express();
+const cors = require('cors');
+
+app.use(cors());
 
 app.get("/questions", (req, res) => {
 	admin
